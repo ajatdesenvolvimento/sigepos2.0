ab2b25ef00c648472b2fa10db46e9ca956a307d3
alcidesjunior <user.amil>
2016-09-20 12:40:56 -0300

    lista de categorias sendo modelada

--- modules/categorias/categorias.js
+++ modules/categorias/categorias.js
@@ -1,3 +1,8 @@
 app.controller('categoriasCtrl',function($scope,$rootScope){
   $rootScope.categoria = "minha categoria";
+  $rootScope.categories = [
+    {category_name: "Notebook",category_note:"apenas notebooks"},
+    {category_name: "Netbook",category_note:"apenas netbooks"},
+    {category_name: "Rede",category_note:"apenas redes"}
+  ];
 });

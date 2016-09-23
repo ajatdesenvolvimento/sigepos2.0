b0597311345ef91988153dc707cdc56dafb8aced
alcidesjunior <user.amil>
2016-09-23 15:45:59 -0300

    tela de listagem de categorias pronta

--- modules/categorias/categorias.js
+++ modules/categorias/categorias.js
@@ -3,6 +3,10 @@ app.controller('categoriasCtrl',function($scope,$rootScope){
   $rootScope.categories = [
     {category_name: "Notebook",category_note:"apenas notebooks"},
     {category_name: "Netbook",category_note:"apenas netbooks"},
-    {category_name: "Rede",category_note:"apenas redes"}
+    {category_name: "Rede",category_note:"apenas redes"},
+    {category_name: "Notebook",category_note:"apenas notebooks"},
+    {category_name: "Netbook",category_note:"apenas netbooks"},
+    {category_name: "Rede",category_note:"apenas redes"},
+    {category_name: "Notebook",category_note:"apenas notebooks"},
   ];
 });
